fc045e27a33efa378a84dd7229660eb6383d1b74
karolplana <karolplana1@gmail.com>
2025-04-15 11:45:13 -0700

    Test changes

--- test_application_setup.py
+++ test_application_setup.py
@@ -6,7 +6,9 @@ nasa_key = st.secrets["nasa_key"]
 def test_get_aopd():
     image_of_the_day = get(f"https://api.nasa.gov/planetary/apod?api_key={nasa_key}").json()
     image_url = image_of_the_day.get("url", None)
+    print(image_url)
     assert image_url is not None
 def test_get_donki():
     notifications = get(f"https://api.nasa.gov/DONKI/notifications?api_key={nasa_key}").json()
+    print(notifications)
     assert notifications is not None
